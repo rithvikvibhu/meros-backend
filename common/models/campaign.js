d62c776e37e39315636ec64bb2c4e86d5fc813ae
Rithvik Vibhu <rithvikvibhu@gmail.com>
2016-12-09 20:36:33 +0530

    Disabled ACL, added new fields

--- common/models/campaign.js
+++ common/models/campaign.js
@@ -2,4 +2,8 @@
 
 module.exports = function(Campaign) {
 
+  Campaign.beforeRemote('create', function(context, user, next) {
+    context.args.data.dateCreated = Date.now();
+    context.args.data.customerId = context.req.accessToken.userId;
+  });
 };
