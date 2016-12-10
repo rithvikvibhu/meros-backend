f55746421fea0b69b89dc32d31fb49500596399d
Rithvik Vibhu <rithvikvibhu@gmail.com>
2016-12-10 09:18:59 +0530

    Revert "Disabled ACL, added new fields"
    This reverts commit d62c776e37e39315636ec64bb2c4e86d5fc813ae.

--- common/models/campaign.js
+++ common/models/campaign.js
@@ -2,8 +2,4 @@
 
 module.exports = function(Campaign) {
 
-  Campaign.beforeRemote('create', function(context, user, next) {
-    context.args.data.dateCreated = Date.now();
-    context.args.data.customerId = context.req.accessToken.userId;
-  });
 };
